2f8fc46cf22fa7d9ef989dc9267a24f234d22a6d
mnjoki21 <mitchellenjoki7@gmail.com>
2022-08-20 12:20:40 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-
+    #setter method
+    def name = (value)
+        @name = value 
+    end
+    #getter method
+    def name
+        @name
+        
+    end
 end
+
+oddie =Dog.new
+
+oddie.name = "Oddie"
+
+puts oddie.name
